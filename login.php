7fc3b15c7e60c2f7c9603440f467f8b9f675001a
Tatiana Koroleva <87785060+tatyanakoroleva00@users.noreply.github.com>
2025-01-23 13:13:59 +0300

    creating a new project

--- login.php
+++ login.php
@@ -1,84 +1,65 @@
 <?php
 session_start();
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+require_once 'models/init.php';
 require_once 'models/functions.php';
 require_once 'controllers/searchUserByEmail.php';
-require_once 'models/categories.php';
-require_once 'models/init.php';
-require_once 'vendor/autoload.php';
-$title = 'Вход';
 
-//Проверка на получение данных из формы
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $form = $_POST;
-    $required = ['email', 'password'];
-    $errors = [];
 
-    //Аутентификация
+# Получение данных
+$form = json_decode(file_get_contents('php://input'), true);
+$required = ['email', 'password'];
+$errors = [];
 
-    $query = "SELECT * from `users`";
-    $result = mysqli_query($con, $query);
-    $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//$_SESSION['test'] = 'test1';
 
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
+# Аутентификация
+$query = "SELECT * from `users`";
+$result = mysqli_query($con, $query);
+$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    // Проверка на пустые значения
-    if (empty($email)) {
-        $errors['email'] = "Пожалуйста, заполните поле email.";
+$email = htmlspecialchars(trim($form['email'])) ?? '';
+$password = htmlspecialchars(trim($form['password'])) ?? '';
+
+# Проверка на пустоту
+if(empty($email)) {
+    $errors['email'] = "Пожалуйста, заполните поле email.";
+}
+if (empty($password)) {
+    $errors['password'] = "Пожалуйста, заполните поле пароль.";
+}
+if(!empty($email) && !empty($password)) {
+# Если нет ошибок и пользователь найден в БД по мейлу
+    if ($user = searchUserByEmail($form['email'], $users)) {
+
+        # Если пароль совпадает с паролем в БД
+        $passwordFromDB = $user['password'];
+
+        if (password_verify($form['password'], $passwordFromDB)) {
+            $_SESSION['user'] = $user;
+//            var_dump($_SESSION['user']);
+        } else { # Если пароль не совпадает с паролем в БД
+            $errors['password'] = 'Неверный пароль';
+            $errors['message'] = 'Вы ввели неверный email/пароль';
+        }
+    } else { # Если такого пользователя нет или есть ошибки
+        $errors['email'] = 'Такой пользователь не найден';
         $errors['message'] = 'Пожалуйста, исправьте ошибки в форме.';
     }
-
-    if (empty($password)) {
-        $errors['password'] = "Пожалуйста, заполните поле пароль.";
-        $errors['message'] = 'Пожалуйста, исправьте ошибки в форме.';
+}
+# Если есть ошибки
+if (count($errors)) {
+    $errors['message'] = 'Пожалуйста, исправьте ошибки в форме.';
+    echo json_encode(['status' => 'error', 'errors' => $errors]);
+}  else {
+    if (isset($_POST['remember']) && $_POST['remember'] === 'on') {
+        setcookie('email', $email, time() + (30 * 24 * 60 * 60), "/"); # cookie хранится 30 дней
     }
-
-    if(!empty($email) && !empty($password)) {
-        //Если нет ошибок и пользователь найден в БД по мейлу
-        if ($user = searchUserByEmail($form['email'], $users)) {
-
-            //Если пароль совпадает с паролем в БД
-            $passwordFromDB = $user['password'];
-
-            if (password_verify($form['password'], $passwordFromDB)) {
-                $_SESSION['user'] = $user;
-
-            } //Если пароль не совпадает с паролем в БД
-            else {
-                $errors['password'] = 'Неверный пароль';
-                $errors['message'] = 'Вы ввели неверный email/пароль';
-            }
-        } //Если такого пользователя нет или есть ошибки
-        else {
-            $errors['email'] = 'Такой пользователь не найден';
-            $errors['message'] = 'Пожалуйста, исправьте ошибки в форме.';
-        }
-    }
-        //Если есть ошибки
-        if (count($errors)) {
-            $page_content = include_template('login.php', [
-                'errors' => $errors,
-                'form' => $form,
-            ]);
-        } //Если нет ошибок, переадресация
-        else {
-            if (isset($_POST['remember']) && $_POST['remember'] === 'on') {
-                setcookie('email', $email, time() + (30 * 24 * 60 * 60), "/"); // cookie хранится 30 дней
-            }
-            header("Location: /index.php");
-            exit();
-        }
-
-} // Если нет запроса POST
-else {
-    $page_content = include_template('login.php', []);
+    echo json_encode(['status' => 'success', 'user' => $_SESSION['user']]);
 }
 
-//Показываем страницу
-$layout_content = include_template('layout.php', [
-    'title' => $title,
-    'content' => $page_content,
-    'categories' => $categories,
-]);
-
-print_r($layout_content);
+//session_write_close();
