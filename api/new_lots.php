1bb6a7744e24ed2bd3c42dfe2f4c753f1313de35
Tatiana Koroleva <87785060+tatyanakoroleva00@users.noreply.github.com>
2025-01-27 14:22:36 +0300

    new and old lots are displayed on the screen

--- api/new_lots.php
+++ api/new_lots.php
@@ -2,8 +2,25 @@
 session_start();
 require_once '../models/init.php';
 
-$stmt = $con->query("SELECT * FROM lot");
-$lots = $stmt->fetch_all(PDO::FETCH_ASSOC);
+//var_dump($_POST);
+$param = json_decode(file_get_contents('php://input'), true);
+$param = $param['show'];
+if($param === 'new') {
+# Сортировка лотов, начиная с тех, чей срок уже почти истек, до тех, у кого поздний срок.
+    $query = "SELECT lot.id, lot.name, lot_message, img_url, lot_rate, lot_date, lot_step, lot.price, cur_price, category.name AS category_name
+        FROM `lot`
+        JOIN category ON lot.category_id = category.id
+        WHERE `lot_date` > NOW()
+        ORDER BY lot_date ASC";
+} else if($param === 'old') {
+    # Закрытые лоты
+    $query = "SELECT lot.id, lot.name, lot_message, img_url, lot_rate, lot_date, lot_step, lot.price, cur_price, category.name AS category_name
+        FROM `lot`
+        JOIN category ON lot.category_id = category.id
+        WHERE `lot_date` < NOW()
+        ORDER BY lot_date ASC";
+}
 
-echo json_encode($lots);
-
+$result = mysqli_query($con, $query);
+$rows = mysqli_fetch_all($result);
+echo json_encode($rows);
