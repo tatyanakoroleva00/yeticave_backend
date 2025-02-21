86075ab11452c239516c87033027b976abac70eb
Tatiana Koroleva <87785060+tatyanakoroleva00@users.noreply.github.com>
2025-02-21 13:39:18 +0300

    work with output of lots by categories, url is being changed to the category name

--- api/categories.php
+++ api/categories.php
@@ -2,8 +2,16 @@
 session_start();
 require_once '../models/init.php';
 
-$stmt = $con->query("SELECT name FROM category");
+//$stmt1 = $con->query("SELECT name_eng FROM category");
+//$categories_eng = $stmt1->fetch_all(PDO::FETCH_ASSOC);
+//
+//$stmt2 = $con->query("SELECT name FROM category");
+//$categories = $stmt2->fetch_all(PDO::FETCH_ASSOC);
+
+$stmt = $con->query("SELECT * FROM category");
 $categories = $stmt->fetch_all(PDO::FETCH_ASSOC);
 
 // Возвращаем данные в формате JSON
+//echo json_encode(['names' => $categories, 'names_eng' => $categories_eng]);
+
 echo json_encode($categories);
--- api/lots_by_categories.php
+++ api/lots_by_categories.php
@@ -1,1 +1,21 @@
 <?php
+session_start();
+require_once '../models/init.php';
+
+
+if ($_SERVER['REQUEST_METHOD' === $_GET]) {
+    echo 'true';
+} else echo 'false';
+//$category = json_decode($_GET['category']);
+//echo json_encode($category);
+//exit;
+
+
+//$stmt = $con->prepare("
+//    SELECT *
+//    FROM lot
+//    JOIN category ON lot.category_id = category.id
+//    WHERE category.name = ?;");
+//$stmt-> execute([$category]);
+//$lots = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//echo json_encode($lots);
