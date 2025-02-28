d84e687e865915a14e38a4eda104b2494b28d173
Tatiana Koroleva <87785060+tatyanakoroleva00@users.noreply.github.com>
2025-02-28 14:34:19 +0300

    site functions, finished with registration

--- sign_up2.php
+++ sign_up2.php
@@ -2,13 +2,14 @@
 session_start();
 require_once 'controllers/searchUserByEmail.php';
 require_once 'models/init.php';
+require_once 'controllers/thumb_func.php';
 
 $required = ['email', 'password', 'name', 'message'];
 $errors = [];
 
 //Проверка на наличие ошибок при заполнении формы
-
 foreach ($required as $field) {
+    // Проверка на пустоту
     if (empty($_POST[$field])) {
         switch ($field) {
             case 'email':
@@ -16,18 +17,18 @@ foreach ($required as $field) {
                 break;
             case 'password':
                 $errors[$field] = 'Введите пароль';
+                break;
             case 'name':
                 $errors[$field] = 'Введите имя';
+                break;
             case 'message':
                 $errors[$field] = 'Напишите как с вами связаться';
+                break;
         }
-        //Проверка на то, что почта неправильно написана (не по формату почты)
+        // Проверка на то, что почта неправильно написана (не по формату почты)
     } else {
         if ($field === 'email') {
-            if (filter_var($_POST[$field], FILTER_VALIDATE_EMAIL)) {
-//                    echo "Адрес электронной почты '$field' является допустимым.";
-            } else {
-//                    echo "Адрес электронной почты '$field' является недопустимым.";
+            if (!filter_var($_POST[$field], FILTER_VALIDATE_EMAIL)) {
                 $errors['email'] = "Адрес электронной почты '$field' является недопустимым.";
             }
         }
@@ -57,13 +58,12 @@ if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] === UPLOAD_ERR_OK) {
     //Проверка формата загруженного файла
     $allowedMimeTypes = ['image/jpeg','image/png',];
 
-
     if(in_array($fileMimeType, $allowedMimeTypes)) {
         $file_name = uniqid() . '-' . $_FILES['avatar']['name'];
-        $file_path = __DIR__ . '/img/newLots/';
-        $file_path_thumb = __DIR__ . '/img/thumbs/';
-        $relative_file_url = '/img/newLots/' . $file_name;
-        $relative_file_url_thumb = '/img/thumbs/' . 'thumb-' . $file_name;
+        $file_path = __DIR__ . '/img/avatars/';
+        $file_path_thumb = __DIR__ . '/img/thumbs_avatars/';
+        $relative_file_url = '/img/avatars/' . $file_name;
+        $relative_file_url_thumb = '/img/thumbs_avatars/' . 'thumb-' . $file_name;
         $uploadFile = $file_path . $file_name;
 
         if(!count($errors)) {
@@ -88,100 +88,28 @@ if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] === UPLOAD_ERR_OK) {
     } else {
         $errors['avatar'] = 'Файл не является допустимым изображением.';
     }
-}
-
-# Ошибки в форме.
-if(count($errors)) {
-    echo json_encode(['file' => $_FILES['avatar'], 'data' => $_POST, 'errors' => $errors]);
 } else {
-    echo json_encode(['file' => $_FILES['avatar'], 'data' => $_POST, 'errors' => $errors]);
+    $_POST['avatar'] = '/img/avatars/avatar.jpg';
 }
 
+//Если после проверок нет ошибок, то показываем определенную страницу
+if (!count($errors)) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $name = $_POST['name'];
+        $message = $_POST['message'];
+        $img_url = $_POST['avatar'];
 
+        $sql = "INSERT INTO `users` SET `email` = ?, `password` = ?, `name` = ?, `contacts` = ?, `avatar` = ?";
+        $stmt = $con->prepare($sql);
+        $stmt->bind_param('sssss', $email, $hashed_password, $name, $message, $img_url);
+        $stmt->execute();
+        $result = $stmt -> get_result();
 
+        echo json_encode(['status' => 'success', 'message' => '*Теперь вы можете войти, используя свой email и пароль']);
 
-////if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] === UPLOAD_ERR_OK) {
-////    // Получение MIME-типа файла
-////    $fileMimeType = mime_content_type($_FILES['avatar']['tmp_name']);
-////    //Проверка формата загруженного файла
-////    $allowedMimeTypes = [
-////        'image/jpeg',
-////        'image/png',
-////    ];
-////
-////    // Проверка, является ли MIME-тип допустимым
-////    if (in_array($fileMimeType, $allowedMimeTypes)) {
-//////            echo "Файл является допустимым изображением.";
-////    } else {
-//////            echo "Файл не является допустимым изображением.";
-////        $errors['avatar'] = 'Файл не является допустимым изображением.';
-////    }
-////
-////    // Занимаемся перемещением файла
-////    $file_name = $_FILES['avatar']['name'];
-////    $file_path = __DIR__ . '/img/avatars/';
-////    $file_url = '/img/avatars/' . $file_name;
-////    $tmp_name = $_FILES['avatar']['tmp_name'];
-////
-////    if (!count($errors)) {
-////        move_uploaded_file($tmp_name, $file_path . $file_name);
-////        $_POST['img_url'] = "$file_url";
-////    }
-////} else {
-////        echo "Файл не был загружен.";
-////}
-//
-////echo json_encode(['errors' => $errors]);
-////var_dump($_FILES);
-////exit();
-//
-//
-////Если после проверок нет ошибок, то показываем определенную страницу
-//if (!count($errors)) {
-////        $email = $_POST['email'];
-////        $password = $_POST['password'];
-////        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-////        $name = $_POST['name'];
-////        $message = $_POST['message'];
-////        $img_url = '';
-////
-////        if(isset($_POST['img_url'])) {
-////        $img_url = $_POST['img_url'];
-////        } else {
-////            $img_url = '/img/avatars/avatar.jpg';
-////        }
-////
-////        $sql = "INSERT INTO `users` SET `email` = ?, `password` = ?, `name` = ?, `contacts` = ?, `avatar` = ?";
-////        $stmt = $con->prepare($sql);
-////        $stmt->bind_param('sssss', $email, $hashed_password, $name, $message, $img_url);
-////        $stmt->execute();
-////
-////        $result = $stmt -> get_result();
-////
-////        echo json_encode(['status' => 'success', 'message' => '*Теперь вы можете войти, используя свой email и пароль']);
-//
-////        $page_content = include_template('login.php', [
-////            'login_page' => $login_page,
-////            'message' => '*Теперь вы можете войти, используя свой email и пароль',
-////        ]);
-//} //Если после проверок есть ошибки, то остаемся на текущей странице
-//else {
-////    echo json_encode(['status' => 'error', 'errors' => $errors]);
-////        $page_content = include_template('sign_up.php', [
-////            'login_page' => $login_page,
-////            'errors' => $errors,
-////        ]);
-//}
-////} else {
-////    $page_content = include_template('sign_up.php', [
-////        'login_page' => $login_page,
-////    ]);
-////}
-////
-////$layout_content = include_template('layout.php', [
-////    'content' => $page_content,
-////    'title' => $title,
-////    'categories' => $categories,
-////]);
-////print($layout_content);
-//
+} //Если после проверок есть ошибки, то остаемся на текущей странице
+else {
+    echo json_encode(['file' => $_FILES['avatar'], 'data' => $_POST, 'errors' => $errors]);
+}
